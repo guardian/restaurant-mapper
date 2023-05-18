39633377eddaa96782a08fa2fb6e30e1dcc8bfc2
Michael Clapham <michaeljclapham@gmail.com>
2023-05-18 11:51:26 +0100

    Improve title styling

--- map-ui/src/TitleBar.tsx
+++ map-ui/src/TitleBar.tsx
@@ -2,7 +2,7 @@ import gRoundelLogo from './Roundel_ white_on_blue_1.svg';
 
 export function TitleBar() {
     return <header className="titleBar">
-        <img src={gRoundelLogo} height="50px"/>
+        <img src={gRoundelLogo}/>
         <h1>Restaurant Review Map</h1>
     </header>
 }
