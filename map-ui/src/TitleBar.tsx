34ffc21b57d8bcde801f15fed83e74ab0045673a
Michael Clapham <michaeljclapham@gmail.com>
2023-05-18 11:29:42 +0100

    Guardian logo in title bar

--- map-ui/src/TitleBar.tsx
+++ map-ui/src/TitleBar.tsx
@@ -1,5 +1,8 @@
+import gRoundelLogo from './Roundel_ white_on_blue_1.svg';
+
 export function TitleBar() {
-    return <header style={{height: "80px", width: "100vw"}}>
+    return <header className="titleBar">
+        <img src={gRoundelLogo} height="50px"/>
         <h1>Restaurant Review Map</h1>
     </header>
 }
