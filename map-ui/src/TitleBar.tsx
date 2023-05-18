ec9c1b0fb2ef62f1e6f7c5278fad70cc612ed64f
Michael Clapham <michaeljclapham@gmail.com>
2023-05-18 12:41:25 +0100

    Play radar sound effect when switching to jaydar

--- map-ui/src/TitleBar.tsx
+++ map-ui/src/TitleBar.tsx
@@ -1,6 +1,7 @@
 import gRoundelLogo from './Roundel_ white_on_blue_1.svg';
 import jayRoundel from './jay_roundel1.png';
 import radarLogo from './radar1.svg';
+import sonarAudioFile from './sonar-ping-95840.mp3';
 
 export interface TitleBarProps {
     jayRadarActivated: boolean;
@@ -8,13 +9,18 @@ export interface TitleBarProps {
 }
 
 export function TitleBar(props: TitleBarProps) {
+    let radarAudio = new Audio(sonarAudioFile);
+
     return <header className="titleBar">
         <img className="guRoundel" src={props.jayRadarActivated ? jayRoundel : gRoundelLogo}/>
         <h1>{props.jayRadarActivated ? "Jay-Radar" : "Restaurant Review Map"}</h1>
         <img
             src={radarLogo}
             className={"radarIcon" + (props.jayRadarActivated ? " activated": "")}
-            onClick={() => props.setJayRadarActivated(!props.jayRadarActivated)}
+            onClick={() => {
+                props.setJayRadarActivated(!props.jayRadarActivated);
+                radarAudio.play();
+            }}
         />
     </header>
 }
